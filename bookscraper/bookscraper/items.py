99b96c1c5c0cccda153c85fd37de3a28cf794b72
ivansuli <galarzaivan016@gmail.com>
2024-06-22 18:04:35 -0300

    first scraping project

---
+++ bookscraper/bookscraper/items.py
@@ -0,0 +1,24 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class BookscraperItem(scrapy.Item):
+    # define the fields for your item here like:
+    name = scrapy.Field()
+    pass
+
+
+class BookItem(scrapy.Item):
+    url = scrapy.Field()
+    title = scrapy.Field()
+    price = scrapy.Field()
+    author = scrapy.Field()
+    illustrator = scrapy.Field()
+    introduceder = scrapy.Field()
+    description = scrapy.Field()
+    image= scrapy.Field()
+
